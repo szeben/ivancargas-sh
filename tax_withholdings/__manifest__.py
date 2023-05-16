c6cee42152334c5f1923db033546d0f518eee3b9
szeben <juliusjaeger@gmail.com>
2023-05-15 20:29:07 -0400

    update taxes

--- tax_withholdings/__manifest__.py
+++ tax_withholdings/__manifest__.py
@@ -21,7 +21,7 @@
     # Check https://github.com/odoo/odoo/blob/14.0/odoo/addons/base/data/ir_module_category_data.xml
     # for the full list
     'category': 'Account',
-    'version': '15.0.2',
+    'version': '0.3',
 
     # any module necessary for this one to work correctly
     'depends': ['base', 'account'],
